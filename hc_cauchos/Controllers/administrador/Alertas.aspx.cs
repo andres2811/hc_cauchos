61c041035f25bd048a023ce76a7f843d4766fe66
ivaaan06 <62316002+ivaaan06@users.noreply.github.com>
2020-04-30 21:27:02 -0500

    Commit Alertas
    Creacion de alertas

---
+++ hc_cauchos/Controllers/administrador/Alertas.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Views_administrador_Alertas : System.Web.UI.Page
+{
+    protected int cont = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+        
+    }
+
+    protected void RepeaterAlerta_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        
+       
+    }
+}
