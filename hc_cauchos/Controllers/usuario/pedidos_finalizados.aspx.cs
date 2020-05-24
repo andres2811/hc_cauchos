82e1cb30073f0dd04534f87daf6b21226f3eef75
ivaaan06 <62316002+ivaaan06@users.noreply.github.com>
2020-05-24 13:07:57 -0500

    Commit factura

--- hc_cauchos/Controllers/usuario/pedidos_finalizados.aspx.cs
+++ hc_cauchos/Controllers/usuario/pedidos_finalizados.aspx.cs
@@ -23,4 +23,12 @@ public partial class Views_usuario_pedidos_finalizados : System.Web.UI.Page
         int idusu = ((EncapUsuario)Session["Valido"]).User_id;
         Session["clienid"] = idusu;
     }
+
+    protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+    {
+        var btn_factura = e.Item.FindControl("Btn_factura") as Button;
+        var lb_idfac = e.Item.FindControl("Id") as Label;
+        Session["Id_fac"] = Convert.ToInt32(lb_idfac.Text);
+        Response.Redirect("Factura.aspx");
+    }
 }
